883e91a8ed830a5ddc173fe93d7232ce4f22e65f
tkkwa01 <aoisyanok@gmail.com>
2024-04-14 20:16:16 +0900

    ssh接続できるようになった

--- main.go
+++ main.go
@@ -1,26 +1,69 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/rivo/tview"
+	"golang.org/x/crypto/ssh"
 )
+
+// SSH接続とコマンド実行の関数
+func connectAndListFiles(username, password, server string) (string, error) {
+	// SSHクライアント設定
+	config := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // 本番環境では適切なホストキーチェックを行うこと
+	}
+
+	// SSHクライアント接続
+	client, err := ssh.Dial("tcp", server+":22", config)
+	if err != nil {
+		return "", err
+	}
+	defer client.Close()
+
+	// SSHセッションの作成
+	session, err := client.NewSession()
+	if err != nil {
+		return "", err
+	}
+	defer session.Close()
+
+	// コマンド実行
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run("ls -l"); err != nil {
+		return "", err
+	}
+
+	return b.String(), nil
+}
 
 func main() {
 	app := tview.NewApplication()
+	form := tview.NewForm()
 
-	// フォームを作成
-	form := tview.NewForm().
-		AddInputField("Username", "k", 20, nil, nil).
-		AddPasswordField("Password", "", 20, '*', nil).
+	var username, password, server string
+	form.AddInputField("Server", "", 20, nil, func(text string) { server = text }).
+		AddInputField("Username", "", 20, nil, func(text string) { username = text }).
+		AddPasswordField("Password", "", 20, '*', func(text string) { password = text }).
 		AddButton("Connect", func() {
-			// SSH接続ロジック
+			output, err := connectAndListFiles(username, password, server)
+			if err != nil {
+				fmt.Println("Error connecting to server:", err)
+				return
+			}
+			fmt.Println("Server response:", output)
 		}).
 		AddButton("Quit", func() {
 			app.Stop()
 		})
 
-	form.SetBorder(true).SetTitle("Enter SSH Credentials").SetTitleAlign(tview.AlignLeft)
+	form.SetBorder(true).SetTitle("SSH Login").SetTitleAlign(tview.AlignLeft)
 
-	// メインループを実行
 	if err := app.SetRoot(form, true).Run(); err != nil {
 		panic(err)
 	}
